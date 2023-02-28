c1a53abe71c0bf9ced648b6a9dd65a044555d6f6
Tri Le <trile95.it@gmail.com>
2023-02-27 22:04:46 -0500

    move all files to server directory

--- server.js
+++ server.js
@@ -11,7 +11,7 @@
  * Module dependencies.
  */
 
-var app = require('./config/app');
+var app = require('./server/config/app');
 var debug = require('debug')('week-3:server');
 var http = require('http');
 
